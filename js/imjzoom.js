6b960f1b0e4c6c22467c6ada33368f1186b7cb55
Iraqez <vladimir.gumenyuk@gmail.com>
2015-10-24 04:08:25 +0300

    1

---
+++ js/imjzoom.js
@@ -0,0 +1,13 @@
+/**
+ * Created by iraqez on 24.10.15.
+ */
+
+$(document).ready(function() {
+    $('.jqzoom').jqzoom({
+        zoomType: 'standard',
+        lens:true,
+        preloadImages: false,
+        alwaysOn:false
+    });
+
+});
